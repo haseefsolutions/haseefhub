d5304f2357524a6b873964161432bf231feb4f85
haseefsolutions <haseefsolutions@outlook.com>
2024-11-07 01:25:10 +0300

    adding linked in dist

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -55,7 +55,7 @@ const enter = (el) => {
               <a href="https://www.linkedin.com/company/haseef-solutions" class="link-offset-2 link-underline link-underline-opacity-0">
               <div  id="link" class="d-flex text-center rounded-pill mt-4 text-light align-items-center position-relative"
                     style="height: 80px;">
-                    <img src="/haseefhub/images/linkedin.png" class=" position-absolute p-2 h-100 w-auto img-fluid rounded-circle"/>
+                    <img src="/images/linkedin.png" class=" position-absolute p-2 h-100 w-auto img-fluid rounded-circle"/>
                     <div class="w-50 mx-auto text-center">
                         <p style="font-size:15px" class=" mb-0 text-wrap"> our linkedIn</p>
 
